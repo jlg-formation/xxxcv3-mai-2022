442fb07ac6ded78745ed0850ed41303f8c8453fe
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-09 14:37:33 +0200

    dev: github users ok

--- back/server.js
+++ back/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
+const axios = require("axios").default;
 
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/github-users", (req, res) => {
+  axios
+    .get("https://api.github.com/users")
+    .then(function (response) {
+      // handle success
+      console.log("ok", response.data);
+
+      res.json(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      res.status(500).end();
+    });
 });
 
 app.listen(port, () => {
