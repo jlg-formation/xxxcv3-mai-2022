83d62f02a7ce98a477c967e5255d56a324c1f529
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-09 15:22:39 +0200

    dev: added redis with connection

--- back/server.js
+++ back/server.js
@@ -1,28 +1,60 @@
 const express = require("express");
 const axios = require("axios").default;
 const morgan = require("morgan");
+const { createClient } = require("redis");
 
-const app = express();
-const port = 3000;
+const client = createClient();
 
-app.use(morgan("tiny"));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+(async () => {
+  await client.connect();
+  console.log("successfully connected to redis");
 
-app.get("/github-users", (req, res) => {
-  (async () => {
-    try {
-      const response = await axios.get("https://api.github.com/users");
-      res.json(response.data);
-    } catch (err) {
-      console.log("err: ", err);
-      res.status(500).end();
+  await client.set("key", "value");
+  const value = await client.get("key");
+
+  const app = express();
+  const port = 3000;
+
+  async function getFromCache(url) {
+    // va dans redis et ramene le resultat si existant.
+    return null;
+  }
+
+  async function setToCache(url, data) {
+    // va dans redis et cree une cle url qui pointe sur data
+  }
+
+  async function cacheGet(url) {
+    const data = await getFromCache(url);
+    if (data) {
+      return data;
     }
-  })();
-});
+    const response = await axios.get(url);
+    setToCache(url, response.data);
+    return response.data;
+  }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  app.use(morgan("tiny"));
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+
+  app.get("/github-users", (req, res) => {
+    (async () => {
+      try {
+        const data = await cacheGet("https://api.github.com/users");
+        res.json(data);
+      } catch (err) {
+        console.log("err: ", err);
+        res.status(500).end();
+      }
+    })();
+  });
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+})();
