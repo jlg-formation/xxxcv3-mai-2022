5c99e9d0bc31a1756a9c38af8a8665f755e6259c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2022-05-09 14:48:41 +0200

    dev: transform promise to async await

--- back/server.js
+++ back/server.js
@@ -9,19 +9,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/github-users", (req, res) => {
-  axios
-    .get("https://api.github.com/users")
-    .then(function (response) {
-      // handle success
+  (async () => {
+    try {
+      const response = await axios.get("https://api.github.com/users");
       console.log("ok", response.data);
-
       res.json(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
+    } catch (err) {
+      console.log("err: ", err);
       res.status(500).end();
-    });
+    }
+  })();
 });
 
 app.listen(port, () => {
